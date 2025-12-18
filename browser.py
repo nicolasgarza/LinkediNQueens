ed859811e3e99855178a393f9e5ae4c6e54508b9
Nicolas Garza <nicolas.garza@gmail.com>
2025-12-18 17:40:45 -0600

    fetch board state and serialize to array. also get cell ptrs for clicking later.

--- browser.py
+++ browser.py
@@ -12,21 +12,78 @@ class Browser:
     def open(self):
         self.page.goto("https://linkedin.com/games/queens/")
 
-        """
+        # self.log_html_and_frames()
+
+        frame = self.page.frame_locator("iframe[title='games']")
+        frame.get_by_role("button", name="Start game").click()
+
         html = self.page.content()
         with open("page.html", "w", encoding="utf-8") as f:
             f.write(html)
 
-        print("Frames:")
-        for i, frame in enumerate(self.page.frames):
-            print(i, frame.url)
-        """
+        # self.log_html_and_frames()
 
         frame = self.page.frame_locator("iframe[title='games']")
-        button = frame.get_by_role("button", name="Start game")
-        button.click()
+        frame.get_by_role("button", name="Dismiss").click()
+
+        board, cell_pointers = self.get_tiles()
+        print(board)
+        print(cell_pointers)
 
         input("enter to close\n")
+
+    def get_tiles(self):
+        # self.log_html_and_frames()
+
+        frame = self.page.frame_locator("iframe[title='games']")
+        queens_grid = frame.locator("#queens-grid")
+
+        children = queens_grid.locator(":scope > div")
+        board, cell_pointers, curr_row = [[]], [[]], 1
+        for child in children.all():
+            label = child.get_attribute("aria-label")
+            if not label: continue
+
+            label = label.split(" ")
+            has_queen = label[0] == "Queen"
+            color = self.extract_color(label)
+
+            label_row = int(label[label.index("row") + 1].rstrip(","))
+            if label_row == curr_row + 1:
+                board.append([])
+                cell_pointers.append([])
+                curr_row += 1
+
+            board[-1].append((has_queen, color))
+            cell_pointers[-1].append(child)
+
+        return board, cell_pointers
+
+    def extract_color(self, words):
+        color = []
+        collecting = False
+
+        for w in words:
+            if w == "color":
+                collecting = True
+                continue
+
+            if collecting:
+                color.append(w.rstrip(","))
+
+                if w.endswith(","):
+                    break
+
+        return " ".join(color)
+
+    def log_html_and_frames(self):
+        html = self.page.content()
+        with open("page.html", "w", encoding="utf-8") as f:
+            f.write(html)
+
+        print("Frames1:")
+        for i, frame in enumerate(self.page.frames):
+            print(i, frame.url)
 
     def close(self):
         self.browser.close()
